094f67575862e10294d3ecee075e7d95f7a90460
DappProgrammer <DappProgrammer@gmail.com>
2019-08-30 17:47:43 +0530

    Using the input function

--- blockchain.py
+++ blockchain.py
@@ -7,9 +7,16 @@ def get_last_blockchain_value():
 
 def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
-    print(blockchain)
 
 
-add_value(5.5)
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=8.7)
-add_value(3.9, get_last_blockchain_value())
+tx_amount = input('Your transaction amount please: ')
+add_value(tx_amount)
+
+tx_amount = input('Your transaction amount please: ')
+add_value(tx_amount, get_last_blockchain_value(), )
+
+tx_amount = input('Your transaction amount please: ')
+add_value(last_transaction=get_last_blockchain_value(),
+          transaction_amount=tx_amount)
+
+print(blockchain)
