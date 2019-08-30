c1e061001827adeb3b9842348f963ef5eacb313c
DappProgrammer <DappProgrammer@gmail.com>
2019-08-30 17:36:05 +0530

    Working with keyword arguments

--- blockchain.py
+++ blockchain.py
@@ -11,5 +11,5 @@ def add_value(transaction_amount, last_transaction=[1]):
 
 
 add_value(5.5)
-add_value(6.3, get_last_blockchain_value())
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=8.7)
 add_value(3.9, get_last_blockchain_value())
