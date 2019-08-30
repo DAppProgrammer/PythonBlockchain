19d4ed158c4f411c30f06046facd4275d25e96e6
DappProgrammer <DappProgrammer@gmail.com>
2019-08-30 17:32:15 +0530

    Using default arguments

--- blockchain.py
+++ blockchain.py
@@ -5,11 +5,11 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_value(transaction_amount):
-    blockchain.append([get_last_blockchain_value(), transaction_amount])
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
     print(blockchain)
 
 
 add_value(5.5)
-add_value(6.3)
-add_value(3.9)
+add_value(6.3, get_last_blockchain_value())
+add_value(3.9, get_last_blockchain_value())
