f39b81afd79cc8f72fed7d9ea2cc16c13a6d0b36
DappProgrammer <DappProgrammer@gmail.com>
2019-08-30 19:18:46 +0530

    Creating while loop

--- blockchain.py
+++ blockchain.py
@@ -24,16 +24,15 @@ def get_user_input():
 tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value(), )
+while True:
+    tx_amount = get_user_input()
+    add_value(last_transaction=get_last_blockchain_value(),
+              transaction_amount=tx_amount)
 
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(),
-          transaction_amount=tx_amount)
-
-for block in blockchain:
-    print('Outputing Block')
-    print(block)
+    # Output the blockchain list to the console
+    for block in blockchain:
+        print('Outputing Block')
+        print(block)
 
 
 print('Done!!!')
